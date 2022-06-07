019fba4dcc69449e79f3fea6cba14312e070a55b
blacktrub <lightlocdog@gmail.com>
2022-06-07 20:36:19 +0300

    catch command errors

--- lua/nvim-typer.lua
+++ lua/nvim-typer.lua
@@ -4,6 +4,14 @@
 local api = vim.api
 local curbuf = 0
 
+local function get_last_command_result()
+    return vim.v.shell_error
+end
+
+local function show_error(text)
+    print("NvimTyper:", text)
+end
+
 local function sub_lines(lines)
     local content = ""
     for _, value in pairs(lines) do
@@ -15,6 +23,11 @@ end
 local function json_to_python_dict(str)
     -- TODO: check errors
     local lines = vim.fn.systemlist(string.format('echo \'%s\' | dict-typer --no-imports', str))
+    if get_last_command_result() ~= 0 then
+        show_error("Json decoding error")
+        return
+    end
+
     return lines
 end
 
@@ -32,6 +45,9 @@ local function nvim_typer()
     local end_block, _ = unpack(api.nvim_buf_get_mark(curbuf, '>'))
     local json = read_current_buf(start_block - 1, end_block)
     local out_lines = json_to_python_dict(json)
+    if not out_lines then
+        return
+    end
     replace_cur_buf(start_block - 1, end_block, out_lines)
 end
 
